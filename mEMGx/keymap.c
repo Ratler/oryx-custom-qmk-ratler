630b4095f8a684ec3041a4c9149bc4374c10efaa
Stefan Wold <ratler@stderr.eu>
2024-11-05 21:02:07 +0100

    fix: move it to achordiion.c

--- mEMGx/keymap.c
+++ mEMGx/keymap.c
@@ -198,31 +198,6 @@ void matrix_scan_user(void) {
   achordion_task();
 }
 
-
-bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t *tap_hold_record,
-                     uint16_t other_keycode, keyrecord_t *other_record) {
-  // Also allow same-hand holds when the other key is in the rows outside the
-  // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboards are split.
-  uint8_t row = other_record->event.key.row % (MATRIX_ROWS / 2);
-  if (!(1 <= row && row <= 3)) {
-    return true;
-  }
-
-  switch (tap_hold_keycode) {
-  // Exceptionally allow symbol layer LTs + row 0 in same-hand chords.
-  case KC_BSPC:
-  case KC_ESCAPE:
-    return true;
-    break;
-    // Exceptionally allow G + J as a same-hand chord.
-    // case NUM_G:
-    //  if (other_keycode == KC_J) { return true; }
-    //  break;
-  }
-
-  return achordion_opposite_hands(tap_hold_record, other_record);
-}
-
 void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
     tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
 
